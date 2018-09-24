2214a3b5c11affec96e20a8909078af8e3d210a2
Christoph Dörfel <christoph.doerfel@gmail.com>
2018-09-24 17:59:44 +0200

    Add index file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { guid } from './utilities/guid';
+export { JwtPayload, JwtUtility } from './utilities/jwt-utilty';
+export { Storage } from './storage/storage';
+export { LocalStorage } from './storage/local.storage';
+export { SessionStorage } from './storage/session.storage';
+export * from './adal.service';
